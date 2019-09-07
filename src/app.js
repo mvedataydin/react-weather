8b86699d8b17f2c4ef732ec2b6ccdf6af01e3708
vedataydin <mvedataydin@gmail.com>
2019-09-07 20:44:40 +0300

    workplace initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.css';
+
+export default class App extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
