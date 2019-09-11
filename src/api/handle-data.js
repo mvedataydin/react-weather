ba0861a3602b1a49ca4174a26dde7db45d90e3b2
vedataydin <mvedataydin@gmail.com>
2019-09-11 14:30:59 +0300

    relocate api modules

--- src/api/handle-data.js
+++ src/api/handle-data.js
@@ -14,9 +14,7 @@ export const dataHandler = weatherData => {
   let cityName = weatherData.city.name;
   let countryCode = weatherData.city.country;
 
-  let currentTime = weatherData.list[0].dt_txt
-    .split(' ')[1]
-    .split(':')[0];
+  let currentTime = weatherData.list[0].dt_txt.split(' ')[1].split(':')[0];
   // calculate the array distance between current data and next day data
   // weather data from api contains data of 3h periods
   let difference = (24 - currentTime) / 3 + 4;
@@ -24,11 +22,8 @@ export const dataHandler = weatherData => {
   const data = [
     {
       day: 1,
-      weekDay: weekDays[dayCode],
-      currentTemp: parseInt(
-        weatherData.list[0].main.temp - 273.15,
-        10
-      ),
+      weekDay: weekDays[dayCode % 7],
+      currentTemp: parseInt(weatherData.list[0].main.temp - 273.15, 10),
       status: weatherData.list[0].weather[0].main,
       humidity: weatherData.list[0].main.humidity,
       windSpeed: weatherData.list[0].wind.speed,
@@ -39,15 +34,9 @@ export const dataHandler = weatherData => {
     },
     {
       day: 2,
-      weekDay: weekDays[dayCode + 1],
-      noonTemp: parseInt(
-        weatherData.list[difference].main.temp - 273.15,
-        10
-      ),
-      nightTemp: parseInt(
-        weatherData.list[difference + 4].main.temp - 273.15,
-        10
-      ),
+      weekDay: weekDays[(dayCode + 1) % 7],
+      noonTemp: parseInt(weatherData.list[difference].main.temp - 273.15, 10),
+      nightTemp: parseInt(weatherData.list[difference + 4].main.temp - 273.15, 10),
       status: weatherData.list[difference].weather[0].main,
       humidity: weatherData.list[difference].main.humidity,
       windSpeed: weatherData.list[difference].wind.speed,
@@ -58,15 +47,9 @@ export const dataHandler = weatherData => {
     },
     {
       day: 3,
-      weekDay: weekDays[dayCode + 2],
-      noonTemp: parseInt(
-        weatherData.list[difference + 8].main.temp - 273.15,
-        10
-      ),
-      nightTemp: parseInt(
-        weatherData.list[difference + 12].main.temp - 273.15,
-        10
-      ),
+      weekDay: weekDays[(dayCode + 2) % 7],
+      noonTemp: parseInt(weatherData.list[difference + 8].main.temp - 273.15, 10),
+      nightTemp: parseInt(weatherData.list[difference + 12].main.temp - 273.15, 10),
       status: weatherData.list[difference + 8].weather[0].main,
       humidity: weatherData.list[difference + 8].main.humidity,
       windSpeed: weatherData.list[difference + 8].wind.speed,
@@ -77,15 +60,9 @@ export const dataHandler = weatherData => {
     },
     {
       day: 4,
-      weekDay: weekDays[dayCode + 3],
-      noonTemp: parseInt(
-        weatherData.list[difference + 16].main.temp - 273.15,
-        10
-      ),
-      nightTemp: parseInt(
-        weatherData.list[difference + 20].main.temp - 273.15,
-        10
-      ),
+      weekDay: weekDays[(dayCode + 3) % 7],
+      noonTemp: parseInt(weatherData.list[difference + 16].main.temp - 273.15, 10),
+      nightTemp: parseInt(weatherData.list[difference + 20].main.temp - 273.15, 10),
       status: weatherData.list[difference + 16].weather[0].main,
       humidity: weatherData.list[difference + 16].main.humidity,
       windSpeed: weatherData.list[difference + 16].wind.speed,
@@ -96,15 +73,9 @@ export const dataHandler = weatherData => {
     },
     {
       day: 5,
-      weekDay: weekDays[dayCode + 3],
-      noonTemp: parseInt(
-        weatherData.list[difference + 24].main.temp - 273.15,
-        10
-      ),
-      nightTemp: parseInt(
-        weatherData.list[difference + 28].main.temp - 273.15,
-        10
-      ),
+      weekDay: weekDays[(dayCode + 4) % 7],
+      noonTemp: parseInt(weatherData.list[difference + 24].main.temp - 273.15, 10),
+      nightTemp: parseInt(weatherData.list[difference + 28].main.temp - 273.15, 10),
       status: weatherData.list[difference + 24].weather[0].main,
       humidity: weatherData.list[difference + 24].main.humidity,
       windSpeed: weatherData.list[difference + 24].wind.speed,
@@ -120,7 +91,5 @@ export const dataHandler = weatherData => {
 
 const calcRainProb = (dataList, status) => {
   let cloud = dataList.clouds.all;
-  return status === 'Rain' && cloud >= 25
-    ? 'Highly probable'
-    : 'Unlikely';
+  return status === 'Rain' && cloud >= 25 ? 'Highly probable' : 'Unlikely';
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import { fetchData } from './components/fetch-data.js';
+import { fetchData } from './api/fetch-data.js';
+import { dataHandler } from './api/handle-data.js';
 
-fetchData();
+const operate = async () => {
+  let data = await fetchData('adana');
+  let faca = dataHandler(data);
+  console.log(data);
+  console.log(faca);
+};
+operate();
+
 ReactDOM.render(<App />, document.getElementById('root'));
