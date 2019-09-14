12aea74aeb0e81eb791830a0b9d76a3466565204
vedataydin <mvedataydin@gmail.com>
2019-09-14 22:24:58 +0300

    add fetch current weather from api and handle that data

--- src/api/fetch-weather-data.js
+++ src/api/fetch-weather-data.js
@@ -1,9 +1,17 @@
 export const fetchWeatherData = async cityName => {
-  const response = await fetch(
+  //FETCH 5 DAY / 3HOURS FORECAST DATA
+  const responseForecast = await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=4a12e1a24af4c1c3362bdec84315696c`,
     { mode: 'cors' }
   );
-
-  const weatherData = await response.json();
-  return weatherData;
+  const forecastData = await responseForecast.json();
+  //FETCH CURRENT WEATHER DATA
+  const responseWeather = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4a12e1a24af4c1c3362bdec84315696c`,
+    { mode: 'cors' }
+  );
+  const weatherData = await responseWeather.json();
+  console.log(forecastData);
+  console.log(weatherData);
+  return [forecastData, weatherData];
 };
--- src/api/handle-data.js
+++ src/api/handle-data.js
@@ -1,4 +1,4 @@
-export const dataHandler = weatherData => {
+export const dataHandler = data => {
   const day = new Date();
   const dayCode = day.getDay();
   const weekDays = [
@@ -10,85 +10,81 @@ export const dataHandler = weatherData => {
     'Friday',
     'Saturday'
   ];
-  let cityName = weatherData.city.name;
-  let countryCode = weatherData.city.country;
+  let cityName = data[0].city.name;
+  let countryCode = data[0].city.country;
   let currentDate = getFormattedDate();
-
-  let currentTime = weatherData.list[0].dt_txt.split(' ')[1].split(':')[0];
+  let currentTime = data[0].list[0].dt_txt.split(' ')[1].split(':')[0];
   // calculate the array distance between current data and next day data
   // weather data from api contains data of 3h periods
   let difference = (24 - currentTime) / 3 + 4;
   if (difference === 12) {
     difference = 11;
   }
-  const data = [
+  const dataFactored = [
     {
       day: 1,
       weekDay: weekDays[dayCode % 7],
-      currentTemp: parseInt(weatherData.list[0].main.temp - 273.15, 10),
-      status: getFormattedStatus(weatherData.list[0].weather[0].main),
-      humidity: weatherData.list[0].main.humidity,
-      windSpeed: weatherData.list[0].wind.speed,
-      rainProbability: calcRainProb(
-        weatherData.list[0],
-        weatherData.list[0].weather[0].main
-      ),
+      currentTemp: parseInt(data[1].main.temp - 273.15, 10),
+      status: getFormattedStatus(data[1].weather[0].main),
+      humidity: data[1].main.humidity,
+      windSpeed: data[1].wind.speed,
+      rainProbability: calcRainProb(data[1], data[1].weather[0].main),
       date: currentDate
     },
     {
       day: 2,
       weekDay: weekDays[(dayCode + 1) % 7],
-      noonTemp: parseInt(weatherData.list[difference].main.temp - 273.15, 10),
-      nightTemp: parseInt(weatherData.list[difference + 4].main.temp - 273.15, 10),
-      status: getFormattedStatus(weatherData.list[difference].weather[0].main),
-      humidity: weatherData.list[difference].main.humidity,
-      windSpeed: weatherData.list[difference].wind.speed,
+      noonTemp: parseInt(data[0].list[difference].main.temp - 273.15, 10),
+      nightTemp: parseInt(data[0].list[difference + 4].main.temp - 273.15, 10),
+      status: getFormattedStatus(data[0].list[difference].weather[0].main),
+      humidity: data[0].list[difference].main.humidity,
+      windSpeed: data[0].list[difference].wind.speed,
       rainProbability: calcRainProb(
-        weatherData.list[difference],
-        weatherData.list[difference].weather[0].main
+        data[0].list[difference],
+        data[0].list[difference].weather[0].main
       )
     },
     {
       day: 3,
       weekDay: weekDays[(dayCode + 2) % 7],
-      noonTemp: parseInt(weatherData.list[difference + 8].main.temp - 273.15, 10),
-      nightTemp: parseInt(weatherData.list[difference + 12].main.temp - 273.15, 10),
-      status: getFormattedStatus(weatherData.list[difference + 8].weather[0].main),
-      humidity: weatherData.list[difference + 8].main.humidity,
-      windSpeed: weatherData.list[difference + 8].wind.speed,
+      noonTemp: parseInt(data[0].list[difference + 8].main.temp - 273.15, 10),
+      nightTemp: parseInt(data[0].list[difference + 12].main.temp - 273.15, 10),
+      status: getFormattedStatus(data[0].list[difference + 8].weather[0].main),
+      humidity: data[0].list[difference + 8].main.humidity,
+      windSpeed: data[0].list[difference + 8].wind.speed,
       rainProbability: calcRainProb(
-        weatherData.list[difference + 8],
-        weatherData.list[difference + 8].weather[0].main
+        data[0].list[difference + 8],
+        data[0].list[difference + 8].weather[0].main
       )
     },
     {
       day: 4,
       weekDay: weekDays[(dayCode + 3) % 7],
-      noonTemp: parseInt(weatherData.list[difference + 16].main.temp - 273.15, 10),
-      nightTemp: parseInt(weatherData.list[difference + 20].main.temp - 273.15, 10),
-      status: getFormattedStatus(weatherData.list[difference + 16].weather[0].main),
-      humidity: weatherData.list[difference + 16].main.humidity,
-      windSpeed: weatherData.list[difference + 16].wind.speed,
+      noonTemp: parseInt(data[0].list[difference + 16].main.temp - 273.15, 10),
+      nightTemp: parseInt(data[0].list[difference + 20].main.temp - 273.15, 10),
+      status: getFormattedStatus(data[0].list[difference + 16].weather[0].main),
+      humidity: data[0].list[difference + 16].main.humidity,
+      windSpeed: data[0].list[difference + 16].wind.speed,
       rainProbability: calcRainProb(
-        weatherData.list[difference + 16],
-        weatherData.list[difference + 16].weather[0].main
+        data[0].list[difference + 16],
+        data[0].list[difference + 16].weather[0].main
       )
     },
     {
       day: 5,
       weekDay: weekDays[(dayCode + 4) % 7],
-      noonTemp: parseInt(weatherData.list[difference + 24].main.temp - 273.15, 10),
-      nightTemp: parseInt(weatherData.list[difference + 28].main.temp - 273.15, 10),
-      status: getFormattedStatus(weatherData.list[difference + 24].weather[0].main),
-      humidity: weatherData.list[difference + 24].main.humidity,
-      windSpeed: weatherData.list[difference + 24].wind.speed,
+      noonTemp: parseInt(data[0].list[difference + 24].main.temp - 273.15, 10),
+      nightTemp: parseInt(data[0].list[difference + 28].main.temp - 273.15, 10),
+      status: getFormattedStatus(data[0].list[difference + 24].weather[0].main),
+      humidity: data[0].list[difference + 24].main.humidity,
+      windSpeed: data[0].list[difference + 24].wind.speed,
       rainProbability: calcRainProb(
-        weatherData.list[difference + 24],
-        weatherData.list[difference + 24].weather[0].main
+        data[0].list[difference + 24],
+        data[0].list[difference + 24].weather[0].main
       )
     }
   ];
-  return [cityName, countryCode, data];
+  return [cityName, countryCode, dataFactored];
 };
 
 const calcRainProb = (dataList, status) => {
